54bbe59c028b66e26f417bde99dfc4da133134f1
Stoon0 <michael.stellner987@gmail.com>
2022-05-01 15:52:39 +0200

    new spawner, added sneaking, added animations

--- RunnerGame/Assets/Scripts/Jump.cs
+++ RunnerGame/Assets/Scripts/Jump.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     public Vector2 jumpHeight;
+    public Animator animator;
+    public GameObject ground;
     private bool touchingObject = false;
 
 
@@ -17,21 +19,42 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (touchingObject && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)))  //makes player jump
+        if (touchingObject && Input.GetButtonDown("Jump"))  //makes player jump
         {
             GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+            animator.SetBool("IsJumping", true);
+        }
+        if (Input.GetButtonDown("Crouch"))  //makes player sneak
+        {
+            animator.SetBool("IsJumping", false);
+            GetComponent<BoxCollider2D>().size = new Vector2(0.2f, 0.2f);
+            GetComponent<Rigidbody2D>().gravityScale = 20;
+            animator.SetBool("IsSneaking", true);
+        }
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            GetComponent<BoxCollider2D>().size = new Vector2(0.2f, 0.4f);
+            GetComponent<Rigidbody2D>().gravityScale = 10;
+            animator.SetBool("IsSneaking", false);
         }
     }
 
     // Collision enter
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        touchingObject = true;
+        if (collision.gameObject.name == ground.name)
+        {
+            animator.SetBool("IsJumping", false);
+            touchingObject = true;
+        }
     }
 
     // Collision exit
     private void OnCollisionExit2D(Collision2D collision)
     {
-        touchingObject = false;
+        if (collision.gameObject.name == ground.name)
+        {
+            touchingObject = false;
+        }
     }
 }
